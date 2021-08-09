8eaeb9259e24f315815520c5a673f7598c900398
alina1030 <85539852+alina1030@users.noreply.github.com>
2021-08-09 10:41:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //*************HALf PAYRAMID ************************//
-/*let n = parseInt(prompt("enter the number")); 
+let n = parseInt(prompt("enter the number")); 
 let string = "";
 for (let i = 1; i <= n; i++) {
   for (let j = 0; j < i; j++) {
@@ -7,14 +7,14 @@ for (let i = 1; i <= n; i++) {
   }
   string += "\n";
 }
-console.log(string);*/
+console.log(string);
 
 //****************HALF INVERTED PYRAMID****************//
 
 let n = parseInt(prompt("enter the number")); // you can take input from prompt or change the value
 let string = "";
 for (let i = 0; i < n; i++) {
-  // printing star
+  
   for (let k = 0; k < n - i; k++) {
     string += "*";
   }
@@ -23,33 +23,32 @@ for (let i = 0; i < n; i++) {
 console.log(string);
 
 //*****************FULL PYRAMID ***************************//
-/*let n = parseInt(prompt("enter the number"))// you can take input from prompt or change the value
+let n = parseInt(prompt("enter the number"))// you can take input from prompt or change the value
 let string = "";
-// External loop
+
 for (let i = 1; i <= n; i++) {
-  // printing spaces
+  
   for (let j = 1; j <= n - i; j++) {
     string += " ";
   }
-  // printing star
+  
   for (let k = 0; k < 2 * i - 1; k++) {
     string += "*";
   }
   string += "\n";
 }
-console.log(string);*/
+console.log(string);
 
 //****************************** Inverted Full Pyramid ***************************/
 
-let n = 5; // you can take input from prompt or change the value
-let string = "";
-// External loop
+let n = 5; 
+
 for (let i = 0; i < n; i++) {
-  // printing spaces
+ 
   for (let j = 0; j < i; j++) {
     string += " ";
   }
-  // printing star
+  
   for (let k = 0; k < 2 * (n-i) - 1; k++) {
     string += "*";
   }
